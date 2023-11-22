346acc431271e997ec536bb487ee406e44de0d61
Sarang <hangpfm0518@ewhain.net>
2023-11-23 03:04:19 +0900

    메인화면 네비바 라우트 주소 수정

--- application.py
+++ application.py
@@ -10,7 +10,23 @@ application.config["SECRET_KEY"] = "Oisobaki"
 
 @application.route("/")
 def main():
-    return render_template("/메인화면.html")
+    return render_template("메인화면.html")
+
+@application.route("/리뷰전체보기")
+def all_review():
+    return render_template("리뷰전체보기.html")
+
+@application.route("/채팅목록")
+def chatlist():
+    return render_template("채팅목록.html")
+
+@application.route("/상품등록")
+def reg_items():
+    return render_template("상품등록.html")
+
+@application.route("/마이페이지1")
+def mypage():
+    return render_template("마이페이지1.html")
 
 if __name__ == "__main__":
  application.run(host='0.0.0.0', debug=True)
